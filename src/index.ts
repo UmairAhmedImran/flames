6c85c98da80086b25a17f93fa258039fb795dcb3
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2026-03-19 01:07:40 +0500

    returning json

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-const welcomeStrings = [
-  'Hello Hono!',
-  'To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono'
-]
+// const welcomeStrings = [
+//   'Hello Hono!',
+//   'To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono'
+// ]
 
-app.get('/', (c) => {
-  return c.text(welcomeStrings.join('\n\n'))
-})
+app.get("/", (c) => {
+  // return c.text(welcomeStrings.join('\n\n'))
+  return c.json({
+    message: "hello, world",
+  });
+});
 
-export default app
+export default app;
